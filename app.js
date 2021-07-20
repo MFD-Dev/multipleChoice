ce50ebf65cd70895c3bf334873433654daaf13a4
Michael <Michaelflowersdev@gmail.com>
2021-07-20 14:38:25 -0500

    initial baseline update

--- app.js
+++ app.js
@@ -1,122 +1,125 @@
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
 
-// create our questions
-const  myQuestions = [
-    {
-    question: "Who is the best javascript mentor?",
-    answers: { 
-    // imgSrc: "img/picture2.png",
-    a: "TJ",
-    b: "Al Einstein",
-    c: "Nik Tesla",
-    d: "John Connor",
-    },
-     correctAnswer: "a"
-},
-    {
-    question: "What is the most popular javascript library or framework?",
-    answers: { 
-    // imgSrc: "img/picture2.png",
-    a: "Angular",
-    b: "Vue",
-    c: "React",
-    d: "Other",
-    },
-     correctAnswer: "c"  
-},
-    {
-    question: "What is the most popular front end programming language?",
-    answers: { 
-    // imgSrc: "img/picture2.png",
-    a: "Html",
-    b: "JavaScript",
-    c: "CSS",
-    d: "Other",
-    },
-    correctAnswer: "b"
-}
- ];
+//Baseline SetUp
+// //Variables
+// const quizContainer = document.getElementById('quiz');
+// const resultsContainer = document.getElementById('results');
+// const submitButton = document.getElementById('submit');
+
+// // create our questions
+// const  myQuestions = [
+//     {
+//     question: "Who is the best javascript mentor?",
+//     answers: { 
+//     // imgSrc: "img/picture2.png",
+//     a: "TJ",
+//     b: "Al Einstein",
+//     c: "Nik Tesla",
+//     d: "John Connor",
+//     },
+//      correctAnswer: "a"
+// },
+//     {
+//     question: "What is the most popular javascript library or framework?",
+//     answers: { 
+//     // imgSrc: "img/picture2.png",
+//     a: "Angular",
+//     b: "Vue",
+//     c: "React",
+//     d: "Other",
+//     },
+//      correctAnswer: "c"  
+// },
+//     {
+//     question: "What is the most popular front end programming language?",
+//     answers: { 
+//     // imgSrc: "img/picture2.png",
+//     a: "Html",
+//     b: "JavaScript",
+//     c: "CSS",
+//     d: "Other",
+//     },
+//     correctAnswer: "b"
+// }
+//  ];
+
+// //Functions
+// function buildQuiz(){
+//   // variable to store the HTML output
+//   const output = [];
+
+//   // for each question...
+//   myQuestions.forEach(
+//     (currentQuestion, questionNumber) => {
+
+//         // console.log('hello')
+//       // variable to store the list of possible answers
+//       const answers = [];
+
+//       // and for each available answer...
+//       for(letter in currentQuestion.answers){
+
+//         // ...add an HTML radio button
+//         answers.push(
+//           `<label>
+//             <input type="radio" name="question${questionNumber}" value="${letter}">
+//             ${letter} :
+//             ${currentQuestion.answers[letter]}
+//           </label>`
+//         );
+//       }
+
+//       // add this question and its answers to the output
+//       output.push(
+//         `<div class="question"> ${currentQuestion.question} </div>
+//         <div class="answers"> ${answers.join('')} </div>`
+//       );
+//     }
+//   );
+
+//   // finally combine our output list into one string of HTML and put it on the page
+//   quizContainer.innerHTML = output.join('');
+// }
 
 
-function buildQuiz(){
-  // variable to store the HTML output
-  const output = [];
+// function showResults() {
+//     //gather answer containers from our quiz
+//     const answerContainers = quizContainer.querySelectorAll('.answers');
 
-  // for each question...
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
+//     //keep track of user's answers
+//     let numCorrect = 0;
 
-        console.log('hello')
-      // variable to store the list of possible answers
-      const answers = [];
+//     //for each question...
+//     myQuestions.forEach((currentQuestion, questionNumber) => {
+//         //find selected answer 
+//         const answerContainer = answerContainers[questionNumber];
+//         const selector = `input[name=question${questionNumber}]:checked`;
+//         const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-      // and for each available answer...
-      for(letter in currentQuestion.answers){
+//         //if answer is correct 
+//         if(userAnswer === currentQuestion.correctAnswer){
+//             //add to the number of correct answers 
+//             numCorrect++;
 
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
+//             //color the answers green
+//             answerContainers[questionNumber].style.color = 'lightgreen';
+//         }
+//         //if answer is wrong or blank
+//         else{
+//             //color the answers red
+//             answerContainers[questionNumber].style.color = 'red';
+//         }
+//         //show number of correct answers out of total
+//         resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+//     });
 
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-      );
-    }
-  );
+// }
 
-  // finally combine our output list into one string of HTML and put it on the page
-  quizContainer.innerHTML = output.join('');
-}
+// //display quiz right away
+// buildQuiz();
 
-
-function showResults() {
-    //gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll('.answers');
-
-    //keep track of user's answers
-    let numCorrect = 0;
-
-    //for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-        //find selected answer 
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).nodeValue;
-
-        //if answer is correct 
-        if(userAnswer === currentQuestion.correctAnswer){
-            //add to the number of correct answers 
-            numCorrect++;
-
-            //color the answers green
-            answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        //if answer is wrong or blank
-        else{
-            //color the answers red
-            answerContainers[questionNumber].style.color = 'red';
-        }
-        //show number of correct answers out of total
-        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    });
-
-}
-
-//display quiz right away
-buildQuiz();
-
-//on submit; show results
-submitButton.addEventListener('click', showResults);
+// //Event Listeners -- on submit; show results
+// submitButton.addEventListener('click', showResults);
 
 
 
